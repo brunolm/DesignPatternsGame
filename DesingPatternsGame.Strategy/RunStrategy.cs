44704f82800bbe7979425c530f106a21c2e7a50e
brunolm7 <brunolm7@gmail.com>
2014-09-23 00:54:18 -0300

    Animation class

--- DesingPatternsGame.Strategy/RunStrategy.cs
+++ DesingPatternsGame.Strategy/RunStrategy.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DesingPatternsGame.Common;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -10,28 +11,21 @@ namespace DesingPatternsGame.Strategy
 {
     public class RunStrategy : IMoveStrategy
     {
+        private Animation animation;
+
+        public RunStrategy()
+        {
+            this.animation = new Animation(100, 48, 48, 5, 0, 48 * 3);
+        }
+
         public Vector2 Move(Vector2 spritePosition, GamePadState controllerState)
         {
             return spritePosition + 7 * new Vector2(controllerState.ThumbSticks.Left.X, -controllerState.ThumbSticks.Left.Y);
         }
-        int frame = 0;
-        double delay = 250;
-        double elapsed = 0;
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Texture2D texture, Vector2 position)
         {
-            spriteBatch.Draw(texture, position, new Rectangle(48 * frame, 144 + 48, 48, 48), Color.White);
-
-            elapsed += gameTime.ElapsedGameTime.TotalMilliseconds;
-
-            if (elapsed > delay)
-            {
-                elapsed = 0;
-                ++frame;
-
-                if (frame >= 6)
-                    frame = 0;
-            }
+            this.animation.Draw(gameTime, spriteBatch, texture, position);
         }
     }
 }
