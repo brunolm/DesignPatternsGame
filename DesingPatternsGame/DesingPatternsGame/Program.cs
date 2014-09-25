de69f41bb9e162436d6bdd63d97d729c543a11f7
brunolm7 <brunolm7@gmail.com>
2014-09-24 21:22:49 -0300

    Game selection through command line

--- DesingPatternsGame/DesingPatternsGame/Program.cs
+++ DesingPatternsGame/DesingPatternsGame/Program.cs
@@ -14,12 +14,53 @@ namespace DesingPatternsGame
     {
         static void Main(string[] args)
         {
-            using (var game = new StrategyGame())
+            var container = new CompositionContainer(new AssemblyCatalog(Assembly.GetExecutingAssembly()));
+            container.ComposeParts(GameIndex.Instance);
+
+            try
             {
-                game.Run();
+                using (var game = GameIndex.Instance[args[0]])
+                {
+                    game.Run();
+                }
+            }
+            catch
+            {
+                using (var game = new ObserverGame())
+                {
+                    game.Run();
+                }
             }
         }
     }
 #endif
+
+    public class GameIndex
+    {
+        [ImportMany(typeof(BaseGame))]
+        private IEnumerable<BaseGame> games = null;
+
+        public BaseGame this[string itemName]
+        {
+            get
+            {
+                return games.FirstOrDefault(o => o.GetType().Name.Equals(itemName, StringComparison.OrdinalIgnoreCase));
+            }
+        }
+
+        private GameIndex()
+        {
+
+        }
+
+        private static GameIndex instance;
+        public static GameIndex Instance
+        {
+            get
+            {
+                return instance ?? (instance = new GameIndex());
+            }
+        }
+    }
 }
 
