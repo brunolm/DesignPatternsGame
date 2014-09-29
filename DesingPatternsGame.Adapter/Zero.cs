3ad574566f21a1bdca9735818b3ca226b6aced4c
brunolm7 <brunolm7@gmail.com>
2014-09-28 21:35:38 -0300

    Adapter Pattern

---
+++ DesingPatternsGame.Adapter/Zero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesingPatternsGame.Adapter
+{
+    public sealed class Zero
+    {
+        public void MoveDashing()
+        {
+            // 3rd party code - unmodifiable
+        }
+    }
+}
