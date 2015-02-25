e162661c4306d6cecb801e7706d5302fdaddaa97
brunolm7 <brunolm7@gmail.com>
2015-02-24 21:19:25 -0300

    Flyweight Pattern

--- DesingPatternsGame.Factory/GameSpriteFactory.cs
+++ DesingPatternsGame.Factory/GameSpriteFactory.cs
@@ -1,8 +1,6 @@
 namespace DesingPatternsGame.Factory
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    using DesingPatternsGame.Common;
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Audio;
     using Microsoft.Xna.Framework.Content;
@@ -10,11 +8,16 @@ namespace DesingPatternsGame.Factory
     using Microsoft.Xna.Framework.Graphics;
     using Microsoft.Xna.Framework.Input;
     using Microsoft.Xna.Framework.Media;
-    using DesingPatternsGame.Common;
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class GameSpriteFactory
     {
         private ContentManager content;
+
+        private readonly ConcurrentDictionary<Type, GameSprite> cache = new ConcurrentDictionary<Type, GameSprite>();
 
         public GameSpriteFactory(ContentManager content)
         {
@@ -23,7 +26,7 @@ namespace DesingPatternsGame.Factory
 
         public GameSprite Create<T>() where T : GameSprite
         {
-            return Activator.CreateInstance(typeof(T), new[] { content }) as T;
+            return this.cache.GetOrAdd(typeof(T), (v) => Activator.CreateInstance(typeof(T), new[] { content }) as T);
         }
     }
 }
